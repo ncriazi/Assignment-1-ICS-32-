b1ed3a5305415cb0e4a392eaca62fb7b50c17ad8
ncriazi <ncriazi@gmail.com>
2025-04-16 16:10:35 -0700

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,27 @@
+# Starter code for assignment 1 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Nick Riazi
+# ncriazi@uci.edu
+# 14622762
+
+def main():
+
+    while True:
+        user_input = input("Enter a valid supported command:\n")
+        command_list = user_input.strip().split(" ")
+        
+        if command_list[0] == "Q":
+            break
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
